b380197f0e7218ad1ba1f1db696a6d000bf1f4cd
vallevaleraasiel <vallevaleraasiel@gmail.com>
2023-12-29 07:56:13 -0600

    Changed some defaultMessage on Home.jsx. I justified and aligned the text of the HomePage on Home.css. In the occupation id, I removed the contraction. I reduced the size of my photo in App.css. I gave a style to the Skill Express' background.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,8 @@ export const Home = () => {
       <div className='container'>
         <div className='home-section__container--text'>
           <h1><FormattedMessage id='my-name-is' defaultMessage='My name is' /><span>Asiel Valle Valera.</span></h1>
-          <h2><FormattedMessage id='occupation' defaultMessage="I'm a Web Developer." /></h2>
-          <p className='text-justify'><FormattedMessage id='explain-occupation' defaultMessage="I'm knowledgeable about a wide range of technologies for software development. I have a passion for creating resilient, scalable, and maintainable software. I'm excited to improve the experiences of the users. I'm meticulous in my attention to the details." /></p>
+          <h2><FormattedMessage id='occupation' defaultMessage='I am a Web Developer.' /></h2>
+          <p className='text-justify'><FormattedMessage id='explain-occupation' defaultMessage='I am knowledgeable about a wide range of technologies for software development. I have a passion for creating resilient, scalable, and maintainable software. I am excited to improve the experiences of the users. I am meticulous in my attention to the details.' /></p>
           <a href='#about' className='about-me-button'><FormattedMessage id='about-me-button' defaultMessage='More about me' /></a>
         </div>
         <div className='home-section__container--my-img'>
